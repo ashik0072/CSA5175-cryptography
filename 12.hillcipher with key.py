7464c805499ca713aa4fb8d3a7368a3186caa4cb
A.C.S <114140750+ashik0072@users.noreply.github.com>
2023-05-15 10:55:10 -0700

    12.hillcipher with key.py

--- 12.hillcipher with key.py
+++ 12.hillcipher with key.py
@@ -1,21 +1,19 @@
-import numpy as np
-
-key = np.array([[9, 4], [5, 7]])
-
-def string_to_matrix(s):
-    return np.array([ord(c) - 97 for c in s])
-
-def matrix_to_string(matrix):
-    return ''.join([chr(int(x) + 97) for x in matrix])
-
-def hill_cipher_encrypt(message):
-    message_matrix = string_to_matrix(message)
-    message_matrix = np.reshape(message_matrix, (-1, 2)).T
-    len = message_matrix.shape[1]
-    encrypted_matrix = np.mod(np.dot(key, message_matrix), 26)
-    encrypted_message = matrix_to_string(encrypted_matrix.T.flatten())
-    return encrypted_message
-
-message = "meet me at the usual place at ten rather than eight oclock"
-encrypted_message = hill_cipher_encrypt(message)
-print(encrypted_message)
+matrix=[[2,3],[3,4]]
+plain=str(input("ENTER THE PLAIN TEXT : "))
+letter="abcdefghijklmnopqrstuvwxyz"
+cipher=""
+array=[]
+after_mul=[]
+for i in plain:
+    if i in letter:
+        pos=letter.find(i)
+        array.append(pos)
+for i in range(0,len(array),2):
+    a,b=array[i],array[i+1]
+    c,d=((a*matrix[0][0])+(b*matrix[1][0])),((a*matrix[0][1])+(b*matrix[1][1]))
+    e,f=(c%26),(d%26)
+    after_mul.append(e)
+    after_mul.append(f)
+for i in after_mul:
+    cipher+=letter[i]
+print(cipher)
